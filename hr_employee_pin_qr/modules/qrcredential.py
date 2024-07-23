022a7fba4ba6ec8eff499356e9943368dd225703
Alexander Labrador <globehackers@gmail.com>
2024-07-23 16:28:10 +0200

    update

---
+++ hr_employee_pin_qr/modules/qrcredential.py
@@ -0,0 +1,28 @@
+from odoo import models, api
+import qrcode
+import base64
+from io import BytesIO
+
+class QrCredential(models.Model):
+    _inherit = 'hr.employee'
+
+    @api.depends('pin')
+    def generate_qr_code(self):
+        for employee in self:
+            if employee.pin:
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+                    box_size=10,
+                    border=4,
+                )
+                qr.add_data(employee.pin)
+                qr.make(fit=True)
+
+                img = qr.make_image(fill='black', back_color='white')
+                buffer = BytesIO()
+                img.save(buffer, format="PNG")
+                qr_image = base64.b64encode(buffer.getvalue())
+                employee.pin = qr_image
+            else:
+                employee.pin = False
