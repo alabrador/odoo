5424f5e9b62795b9ecf568ce6e10cdfe94c1f7a9
Alexander Labrador <globehackers@gmail.com>
2024-07-31 13:59:46 +0200

    update

--- web_booking_product/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_rental = fields.Boolean(string='Is a Rental Product')
-    rental_stock = fields.Integer(string='Rental Stock', default=1)
